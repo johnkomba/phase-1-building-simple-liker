0dcab8381973f12fca814db704e3cd3f0564da7f
John Komba <john.mwangi1@student.moringaschool.com>
2023-10-04 14:14:44 +0300

    done with assignment

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
-
-
-
-
-//------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+  const EMPTY_HEART = '♡';
+  const FULL_HEART = '♥';
+  const modal = document.getElementById('modal');
+  const modalMessage = document.getElementById('modal-message');
+  const likeGlyphs = document.querySelectorAll('.like-glyph');
 // Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+  function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        let isRandomFailure = Math.random() < .2
+        if (isRandomFailure) {
+          reject("Random server error. Try again.");
+        } else {
+          resolve("Pretend remote server notified of action!");
+        }
+      }, 300);
+    });
+  }
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+  likeGlyphs.forEach(likeGlyph => {
+    likeGlyph.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (likeGlyph.innerText === EMPTY_HEART) {
+            likeGlyph.innerText = FULL_HEART;
+            likeGlyph.classList.add('activated-heart');
+          } else {
+            likeGlyph.innerText = EMPTY_HEART;
+            likeGlyph.classList.remove('activated-heart');
+          }
+        })
+        .catch(error => {
+          modalMessage.innerText = error;
+          modal.classList.remove('hidden');
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    });
   });
-}
+
+  modal.querySelector('.modal-close').addEventListener('click', () => {
+    modal.classList.add('hidden');
+  });
+});
